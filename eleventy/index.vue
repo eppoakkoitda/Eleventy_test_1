617e49246815bd0703400c3e650d6fd2de1104a7
eppoakkoitda <eppoakkotida@icloud.com>
2024-02-02 22:56:27 +0900

    test data

---
+++ eleventy/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <article>
+    <h1>This is a test</h1>
+    <ul>
+      <li v-for="(listItem, index) in listItems" :key="index">
+        {{ listItem }}
+      </li>
+    </ul>
+  </article>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "Wow I'm So Excited To Use Vue In My Templates!",
+      listItems: [
+        "This is the first item",
+        "This is the second",
+        "This is perhaps the third, though who could truly say.",
+      ],
+    };
+  },
+};
+</script>
